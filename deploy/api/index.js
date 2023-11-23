a4943f033dc8015dd3c912f956804baafe2382c8
Alexandre <aofferle@cfai-formation.fr>
2023-11-23 16:38:26 +0100

    test

--- deploy/api/index.js
+++ deploy/api/index.js
@@ -4,10 +4,7 @@ const cors = require("cors");
 const app  = express ();
 
 var corsOptions = {
-  origin: "*",
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  headers: 'Content-Type, Authorization',
-  exposedHeaders:'Authorization'
+  origin: "http://localhost:443"
 };
 
 app.use(cors(corsOptions));
@@ -18,13 +15,26 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to CNAM application." });
+});
 
-require("./routes")(app);
+const db = require("./models");
 
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
+require("./routes/utilisateur.routes")(app);
 
 // set port, listen for requests
 const PORT =  443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
